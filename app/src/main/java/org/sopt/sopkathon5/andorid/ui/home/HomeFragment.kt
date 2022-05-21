d9173423be645baf7852934649b9f49855814216
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-05-21 23:04:51 +0900

    [UI] #3 - Init BottomNavigation

---
+++ app/src/main/java/org/sopt/sopkathon5/andorid/ui/home/HomeFragment.kt
@@ -0,0 +1,17 @@
+package org.sopt.sopkathon5.andorid.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.sopt.sopkathon5.andorid.R
+import org.sopt.sopkathon5.andorid.databinding.FragmentHomeBinding
+import org.sopt.sopkathon5.andorid.util.base.BaseFragment
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/org/sopt/sopkathon5/andorid/ui/mypage/MyPageFragment.kt
@@ -0,0 +1,13 @@
+package org.sopt.sopkathon5.andorid.ui.mypage
+
+import android.os.Bundle
+import android.view.View
+import org.sopt.sopkathon5.andorid.R
+import org.sopt.sopkathon5.andorid.databinding.FragmentMyPageBinding
+import org.sopt.sopkathon5.andorid.util.base.BaseFragment
+
+class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
