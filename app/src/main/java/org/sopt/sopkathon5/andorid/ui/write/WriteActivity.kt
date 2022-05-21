b6fd185e787abe988857630ead65b820b201f795
jinsil <wryt9224@gmail.com>
2022-05-22 05:46:35 +0900

    [FEAT] #4 - 기능 구현

--- app/src/main/java/org/sopt/sopkathon5/andorid/ui/write/WriteActivity.kt
+++ app/src/main/java/org/sopt/sopkathon5/andorid/ui/write/WriteActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import org.sopt.sopkathon5.andorid.R
-import org.sopt.sopkathon5.andorid.data.model.RequestWrite
-import org.sopt.sopkathon5.andorid.data.model.ResponseWrite
-import org.sopt.sopkathon5.andorid.data.model.ServiceCreator
-import org.sopt.sopkathon5.andorid.data.model.WriteService
 import org.sopt.sopkathon5.andorid.databinding.ActivityWriteBinding
 import org.sopt.sopkathon5.andorid.util.base.BaseActivity
 import retrofit2.Call
@@ -23,20 +19,17 @@ class WriteActivity : BaseActivity<ActivityWriteBinding>(R.layout.activity_write
         finishView()
 
         binding.btnWriteRegister.setOnClickListener {
-            if(binding.etWriteList01.text.isNullOrBlank()||binding.etWriteList02.text.isNullOrBlank()||binding.etWriteList03.text.isNullOrBlank())
-            {
-                Toast.makeText(this@WriteActivity,"일상을 최소 3가지 공유해주세요", Toast.LENGTH_SHORT).show()
-            }
-            else
-            {
+            if (binding.etWriteList01.text.isNullOrBlank() || binding.etWriteList02.text.isNullOrBlank() || binding.etWriteList03.text.isNullOrBlank()) {
+                Toast.makeText(this@WriteActivity, "일상을 최소 3가지 공유해주세요", Toast.LENGTH_SHORT).show()
+            } else {
                 finish()
             }
         }
     }
 
-    private fun addList(){
-        binding.btnWriteAddList.setOnClickListener{
-            when(cnt) {
+    private fun addList() {
+        binding.btnWriteAddList.setOnClickListener {
+            when (cnt) {
                 0 -> {
                     binding.ivWrite4.visibility = View.VISIBLE
                     binding.isVisible4 = true
@@ -52,18 +45,16 @@ class WriteActivity : BaseActivity<ActivityWriteBinding>(R.layout.activity_write
         }
     }
 
-    private fun finishView(){
-        binding.btnWriteFinish.setOnClickListener{
+    private fun finishView() {
+        binding.btnWriteFinish.setOnClickListener {
             finish()
         }
     }
 
 
-    private fun writeNetWork(){
+    private fun writeNetWork() {
 
     }
-
-
 
 
 }
