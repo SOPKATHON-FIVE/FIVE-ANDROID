07a6a798b82397bd11aa1077f4242ce8abd58325
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-05-22 04:37:43 +0900

    [FEAT] #11 - Impl MyPage

--- app/src/main/java/org/sopt/sopkathon5/andorid/ui/home/DailyAdapter.kt
+++ app/src/main/java/org/sopt/sopkathon5/andorid/ui/home/DailyAdapter.kt
@@ -7,23 +7,26 @@ import androidx.recyclerview.widget.RecyclerView
 import org.sopt.sopkathon5.andorid.R
 import org.sopt.sopkathon5.andorid.data.model.DailyData
 import org.sopt.sopkathon5.andorid.databinding.ItemDailyListBinding
+import kotlin.math.log
 
-class DailyAdapter(private var itemClick: (() -> (Unit))? = null) :
-    RecyclerView.Adapter<DailyAdapter.DailyViewHolder>() {
+class DailyAdapter(private val itemClick: (() -> (Unit))? = null, private val logoutClickListener: (() -> Unit)? = null) : RecyclerView.Adapter<DailyAdapter.DailyViewHolder>() {
     val itemList = mutableListOf<DailyData>()
 
     class DailyViewHolder(
         private val binding: ItemDailyListBinding,
-        private val itemClick: () -> Unit
+        private val itemClick: (() -> Unit)?
     ) :
         RecyclerView.ViewHolder(binding.root) {
-        fun onBind(data: DailyData) {
+        fun onBind(data: DailyData, logoutClickListener: (() -> Unit)?) {
             binding.data = data
+            binding.tvLogout.setOnClickListener {
+                logoutClickListener?.invoke()
+            }
             val todoAdapter = TodoAdapter()
             binding.rvTodo.adapter = todoAdapter
 
             binding.btnPoint.setOnClickListener {  //호출시 itemclick 람다함수실행함
-                itemClick.invoke()
+                itemClick?.invoke()
             }
         }
     }
@@ -39,7 +42,7 @@ class DailyAdapter(private var itemClick: (() -> (Unit))? = null) :
     }
 
     override fun onBindViewHolder(holder: DailyViewHolder, position: Int) {
-        holder.onBind(itemList[position])
+        holder.onBind(itemList[position], logoutClickListener)
     }
 
     override fun getItemCount(): Int = itemList.size
